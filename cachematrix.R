2020a01e30d53215342eeb15a6caff294ad0bb6b
Bjornar <bjornar.arnesen1@gmail.com>
2014-09-15 10:08:40 -0500

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,14 @@
+## The functions in this R script calculates the inverse
+## of an invertible matrix and store the result in cache.
+## When later trying to solve the inverse of a matrix, it
+## first checks if the solution already exists in cache in
+## order to determine the necessity to calculate again.
+
+
+## The makeCacheMatrix function creates a matrix
+## that sets and gets the values of a matrix and
+## then sets and gets the inverse of that matrix.
+
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
   set<-function(y){
@@ -11,6 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
        setmatrix=setmatrix,
        getmatrix=getmatrix)
 }
+
+## The cacheSolve function checks if the calculation of
+## an inverse matrix exists in cache. If exists, a new
+## calculation of the inverse is skipped. If not exist, the
+## inverse is calculated and is set in cache. 
 
 cacheSolve <- function(x=matrix(), ...) {
   m<-x$getmatrix()
